428d8fdd31a8bd384c3b2a5d7ced4e18836aeb98
evertcode <hebert.hernandez7@gmail.com>
2021-09-29 11:08:45 -0500

    2 files modified, 3 added

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-// run `node index.js` in the terminal
+const express = require('express');
+const path = require('path');
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const router = express.Router();
+const app = express();
+
+const PORT = proccess.env.PORT || 13000;
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/views/index.html'));
+});
+
+app.use('/', router);
+app.listening(PORT, () => {
+  console.log('Server running...');
+});
